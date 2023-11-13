fdab59fadffb31414050e7bc1a5f185c7de255eb
ashok choudhary1905 <ashok90olp8@gmail.com>
2023-11-14 00:27:51 +0530

    add crud functions and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,33 @@ class ArticlesController < ApplicationController
     def index 
         @articles=Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def create 
+        @article = Article.new(params.require(:article).permit(:title,:description))
+       if @article.save
+        flash[:notice] = "article was created"
+         redirect_to @article
+       else
+         render 'new' ,status: :unprocessable_entity
+       end
+    end
+    def edit 
+        @article=Article.find(params[:id])
+    end
+    def update 
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+         flash[:notice] = "article was updated"
+          redirect_to @article
+        else
+          render 'edit' ,status: :unprocessable_entity
+        end
+    end
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
 end
